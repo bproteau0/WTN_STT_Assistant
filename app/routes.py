8d876360e8135ea448f783ac17fd7a6011deefe7
Brandon <bproteau@comcast.net>
2025-09-20 09:43:07 -0400

    Added some contents from FMT ch2.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Index Page"
+    user = {'username': 'bproteau0'}
+    return render_template('index.html', title='WTN Assistant', user=user)
