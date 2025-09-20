eeef5a92a8164bb9ee7891ccf0e1098d988fcd9e
Brandon <bproteau@comcast.net>
2025-09-20 13:10:04 -0400

    Added api keys support in route for chat page. See README.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # WTN_STT_Assistant
+
+Created by Brandon Proteau (WPI BS CS '27) for the World Trails Network Americas'/Pan American Trails' Sustainable Trails Toolbox wiki.
+
+## keys.txt
+For the purposes of this project, api keys are handled simply: for app use, create file named "keys.txt".
+Format thusly:
+```
+GEMINI_API_KEY = "Gemini api key here";
+GOOGLE_API_KEY = "programmable search engine api key here";
+SEARCH_ENGINE_ID = "programmable search engine id here";
+```
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,9 @@
 from flask import render_template, redirect
 from app import app
+import re
+
+def extract_key(line):
+    return re.search("\"[^\"]+\"", line)
 
 @app.route("/")
 @app.route("/index")
@@ -9,4 +13,24 @@ def index():
 @app.route("/search")
 @app.route("/chat")
 def chat():
-    return render_template('chat.html')
+    keys_file = open('keys.txt', 'r')
+    
+    gemini_key = extract_key(keys_file.readline())
+    if gemini_key is None:
+        raise Exception("GEMINI_API_KEY not found in keys file")
+    else:
+        gemini_key = gemini_key.group(0)[1:-1]
+
+    google_key = extract_key(keys_file.readline())    
+    if google_key is None:
+        raise Exception("GOOGLE_API_KEY not found in keys file")
+    else:
+        google_key = google_key.group(0)[1:-1]
+        
+    google_id = extract_key(keys_file.readline())
+    if google_id is None:
+        raise Exception("SEARCH_ENGINE_ID not found in keys file")
+    else:
+        google_id = google_id.group(0)[1:-1]
+        
+    return render_template('chat.html', gem_key=gemini_key, ggl_key=google_key, ggl_id=google_id)
